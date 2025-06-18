5ed6aa5c5e57a7458ee52785a1089efa16cbd4b8
HugoZAuad <hugozeymer@hotmail.com>
2025-06-18 12:06:53 -0300

    fix link

--- src/api/LivrosService.js
+++ src/api/LivrosService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "desafio-5-rid-66155-back-end-git-main-hugozauads-projects.vercel.app"
+const BASE_URL = "https://desafio-5-rid-66155-back-end.vercel.app/"
 
 export class LivrosService{
     static getLivros(){
