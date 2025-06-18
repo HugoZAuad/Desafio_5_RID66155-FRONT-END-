190f730ed4d4f1f987b2734cbbedd1f4b6440e6b
HugoZAuad <hugozeymer@hotmail.com>
2025-06-18 12:07:48 -0300

    routes ok

--- src/api/LivrosService.js
+++ src/api/LivrosService.js
@@ -4,23 +4,23 @@ const BASE_URL = "https://desafio-5-rid-66155-back-end.vercel.app/"
 
 export class LivrosService{
     static getLivros(){
-        return axios.get(BASE_URL+'/livros');
+        return axios.get(BASE_URL+'livros');
     }
 
     static getLivro(id){
-        return axios.get(`${BASE_URL}/livros/${id}`);
+        return axios.get(`${BASE_URL}livros/${id}`);
     }
 
     static createLivro(body){
-        return axios.post(`${BASE_URL}/livros`,body);
+        return axios.post(`${BASE_URL}livros`,body);
     }
 
     static updateLivro(id,body){
-        return axios.put(`${BASE_URL}/livros/${id}`,body);
+        return axios.put(`${BASE_URL}livros/${id}`,body);
     }
 
     static deleteLivro(id){
-        return axios.delete(`${BASE_URL}/livros/${id}`);
+        return axios.delete(`${BASE_URL}livros/${id}`);
     }
     
 }
