f8df46b84e31aebb49eb4186ecad77a56ed977eb
HugoZAuad <hugozeymer@hotmail.com>
2025-06-21 23:28:28 -0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import history from 'connect-history-api-fallback'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    middlewareMode: true,
-    setupMiddlewares(middlewares) {
-      middlewares.use(
-        history({
-          index: '/index.html',
-          verbose: true
-        })
-      )
-      return middlewares
-    }
+    open: true
   }
 })
